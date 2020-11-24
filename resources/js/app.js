d36b7f39c18abef0742aca3ed0d808227e2ebd30
Saheb Chhabra <schhabra@Sahebs-MacBook-Pro-2.local>
2020-11-24 13:35:06 -0500

    Added Api's, some modification

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,76 @@
-/* ---------------------------------------------- */
-/*            CODE EXPLAINED TUTORIALS            */
-/*         www.youtube.com/CodeExplained          */
-/* ---------------------------------------------- */
+
+const country_name_element = document.querySelector("country.name");
+const total_cases_elements = document.querySelector(".total-cases .value");
+const new_cases_element = document.querySelector(".total-cases .new-value");
+const recovered_element = document.querySelector(".recovered .value");
+const new_recovered_element = document.querySelector(".recovered .new-value");
+const deaths_element = document.querySelector(".deaths .value");
+const new_deaths_element = document.querySelector(".deaths .new-value");
+
+const ctx = document.getElementById("axes_line_chart").getContext("2d");
+
+
+let app_data = [],
+cases_list =[],
+recovered_list =[],
+deaths_list =[],
+deaths = [];
+
+let country_code = geoplugin_countryCode();
+let user_country;
+country_list.forEach( country =>{
+	if(country.code== country_code){
+		user_country = country.name;
+	}
+});
+
+console.log(user_country);
+
+
+// var url = `https://covid-193.p.rapidapi.com/history?country=${user_country}`
+
+
 
 /* ---------------------------------------------- */
 /*                API URL AND KEY                 */
 /* ---------------------------------------------- */
-/*
-fetch(`https://covid19-monitor-pro.p.rapidapi.com/coronavirus/cases_by_days_by_country.php?country=country`, {
+
+
+function fetchData(user_country){
+	fetch(`https://covid-193.p.rapidapi.com/history?country=${user_country}`,  {
 		"method": "GET",
 		"headers": {
-			"x-rapidapi-host": "covid19-monitor-pro.p.rapidapi.com",
-			"x-rapidapi-key": "7e269ec140msh8a5df9cfc21b4b4p1c1e3ejsn9aba26afc6e0"
+			"x-rapidapi-key": "b0710c71bcmsh1ebd38a2b09d363p1d21fajsn5180f7393dd2",
+			"x-rapidapi-host": "covid-193.p.rapidapi.com",
+			"useQueryString": true
 		}
+	}).then(response =>{
+        return response.json();
+	}).then(data =>{
+
+		console.log(data);
+	// dates = Object.keys(data);	
+	// console.log(dates);
+	 
+	//  dates.forEach( day=>{
+	// 		let Data = data[day];
+
+	// 		app_data.push(Data);
+	// 		console.log(app_data);
+	// 	//    cases_list.push(parseInt(Data.total_cases.replace(/,/g,"")));
+
+	//  })
+
 	})
-*/
+}
+
+
+
+
+
+
+
+
+
+
+fetchData(user_country);
--- resources/js/countries.js
+++ resources/js/countries.js
@@ -204,3 +204,89 @@ let country_list = [
     { name: 'Yemen', code: 'YE' },
     { name: 'China', code: 'CN' }
 ];
+
+
+const search_country_element = document.querySelector(".search-country");
+const country_list_element = document.querySelector(".country-list");
+const chang_country_btn = document.querySelector(".change-country");
+const close_list_btn = document.querySelector(".close");
+const input = document.getElementById('search-input');
+
+//  Create Country List
+
+function createCountryList(){
+    const num_countries = country_list.length;
+
+    let i = 0, ul_list_id;
+
+
+    country_list.forEach( (country, index) => {
+        if(index % Math.ceil(num_countries/num_of_ul_lists) == 0){
+            ul_list_id= `list-${i}`
+            country_list_element.innerHTML +=  `<ul id ='${ul_list_id}'></ul>`;
+            i++;
+        }
+
+        document.getElementById(`${ul_list_id}`).innerHTML += `
+                <li onclick ="fetchData('${country.name}')" id ="${country.name}">
+            ${country.name}
+            </li>
+        `;
+    })
+}
+
+
+let num_of_ul_lists = 3;
+createCountryList();
+
+chang_country_btn.addEventListener("click", function(){
+
+input.value ="";  
+resetCountryList();  
+search_country_element.classList.toggle("hide");
+search_country_element.classList.add("fadein");
+
+})
+
+// close_list_btn.addEventListener("click", function(){
+
+// search_country_element.classList.toggle("hide");
+// })
+
+close_list_btn.addEventListener("click", function(){
+   search_country_element.classList.toggle("hide");
+});
+
+
+country_list_element.addEventListener("click", function(){
+
+    search_country_element.classList.toggle("hide");
+    })
+    
+
+    //  Country filter
+
+
+    input.addEventListener("input", function(){
+        let value = input.value.toUpperCase();
+
+
+        country_list.forEach(country => {
+            if(country.name.toUpperCase().startsWith(value)){
+                document.getElementById(country.name).classList.remove("hide");
+            }
+            else{
+                document.getElementById(country.name).classList.add("hide");
+            }
+        })
+         console.log(value);
+    })
+
+    function resetCountryList(){
+                country_list.forEach(country  => {
+              
+                    document.getElementById(country.name).classList.remove("hide");
+                })
+
+
+    }
